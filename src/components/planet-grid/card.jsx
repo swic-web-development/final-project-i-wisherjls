a723e158223642473ae251d0ad737bd8751468cb
wisher <wisherjls@gmail.com>
2025-05-13 00:28:29 -0500

    added planet card

---
+++ src/components/planet-grid/card.jsx
@@ -0,0 +1,35 @@
+// Render a single planet card.
+export default function Card({ planet }) {
+  const {
+    name,
+    climate,
+    surface_water,
+    diameter,
+    rotation_period,
+    terrain,
+    gravity,
+    orbital_period,
+    population,
+  } = planet // Get planet details.
+
+  return (
+    <section
+      className="overflow-hidden rounded-lg shadow-md"
+      style={{ backgroundColor: '#D2B48C' }} // tan hex
+    >
+      <div className="p-4">
+        <h2 className="text-xl font-semibold text-white">{name}</h2>
+        <div className="mt-2 text-white">
+          <div>Climate: {climate}</div>
+          <div>Terrain: {terrain}</div>
+          <div>Gravity: {gravity}</div>
+          <div>Diameter: {diameter} km</div>
+          <div>Rotation Period: {rotation_period} hours</div>
+          <div>Orbital Period: {orbital_period} days</div>
+          <div>Surface Water: {surface_water}%</div>
+          <div>Population: {population}</div>
+        </div>
+      </div>
+    </section>
+  )
+}
