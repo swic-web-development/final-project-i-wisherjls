9839fe96f1339befb8d9c7d5206fc6abd3440163
wisher <wisherjls@gmail.com>
2025-05-13 00:22:22 -0500

    fetch data from api

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,26 @@
+import Header from './components/header.jsx'
+import PlanetsGrid from './components/planet-grid'
+import { useState, useEffect } from 'react'
+
 export default function App() {
+  const [planets, setPlanets] = useState([])
+
+  useEffect(() => {
+    async function fetchPlanets() {
+      const response = await fetch('https://swapi.tech/api/planets/')
+      const data = await response.json()
+      setPlanets(data.results)
+    }
+    fetchPlanets()
+  }, [])
+
   return (
-    <main>
-      <h1 className="text-3xl text-red-500">Hello, World!</h1>
-      <p>This is a simple React application.</p>
+    <main
+      style={{ backgroundColor: '#136c68' }}
+      className="flex min-h-screen flex-col items-center justify-center"
+    >
+      <Header />
+      <PlanetsGrid planets={planets} />
     </main>
   )
 }
